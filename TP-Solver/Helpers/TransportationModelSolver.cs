4fb835a57e4c4d519ff06698125a10383e6b316b
Antonio Lopac <lopac.antonio@outlook.com>
2017-05-19 17:53:10 +0200

    Add Models, Add NorthWest method algorithm

---
+++ TP-Solver/Helpers/TransportationModelSolver.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TP_Solver.Models;
+
+namespace TP_Solver.Helpers
+{
+    public class TransportationModelSolver
+    {
+        public List<Matrix> SolveNorthWest(Matrix matrix)
+        {
+            var solutionMatrices = new List<Matrix>();
+
+
+            while (matrix.Supplies.Any(x => x != CellState.Allocated) &&
+                   matrix.Demands.Any(x => x != CellState.Allocated))
+            {
+                for (var row = 0; row < matrix.GetLength(0); row++)
+                {
+                    for (var column = 0; column < matrix.GetLength(1); column++)
+                    {
+                        if (matrix[row, column].Allocated is CellState.NotAllocated)
+                        {
+                            matrix.Allocate(row, column);
+                            solutionMatrices.Add(matrix);
+                        }
+                    }
+                }
+            }
+
+            return solutionMatrices;
+        }
+    }
+}
